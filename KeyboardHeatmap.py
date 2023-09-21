6dda005d42d92006e829168f0ab4d947f7cb81c5
JCR101 <crispinroundtree@gmail.com>
2023-09-21 16:49:35 +0100

    added more symbol key functionality

--- KeyboardHeatmap.py
+++ KeyboardHeatmap.py
@@ -21,9 +21,9 @@ canvas = fig.canvas
 keys_pressed = {key: 0 for key in [
     'esc', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12',
     'tilde', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
-    'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'lbracket', 'rbracket', 'backslash',
-    'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'quote', 'enter',
-    'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'period', 'slash', 'rshift',
+    'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+    'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter',
+    'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'rshift',
     'ctrl', 'fn', 'win', 'alt', 'space', 'ralt', 'rctrl', '-'
 ]}
 
@@ -34,11 +34,11 @@ key_visual_representation = {
     'tilde': '~', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7',
     '8': '8', '9': '9', '0': '0', '-': '-', '=': '=', 'backspace': '⬅️',
     'tab': '→|', 'q': 'Q', 'w': 'W', 'e': 'E', 'r': 'R', 't': 'T', 'y': 'Y', 'u': 'U',
-    'i': 'I', 'o': 'O', 'p': 'P', 'lbracket': '[', 'rbracket': ']', 'backslash': '\\',
+    'i': 'I', 'o': 'O', 'p': 'P', '[': '[', ']': ']', '\\': '\\',
     'capslock': '⬆️ Lock', 'a': 'A', 's': 'S', 'd': 'D', 'f': 'F', 'g': 'G', 'h': 'H',
-    'j': 'J', 'k': 'K', 'l': 'L', 'semicolon': ';', 'quote': '\'', 'enter': '⏎',
+    'j': 'J', 'k': 'K', 'l': 'L', ';': ';', '\'': '\'', 'enter': '⏎',
     'shift': '⇧', 'z': 'Z', 'x': 'X', 'c': 'C', 'v': 'V', 'b': 'B', 'n': 'N', 'm': 'M',
-    'comma': ',', 'period': '.', 'slash': '/', 'rshift': '⇧',
+    ',': ',', '.': '.', '/': '/', 'rshift': '⇧',
     'ctrl': 'Ctrl', 'fn': 'Fn', 'win': 'Win', 'alt': 'Alt', 'space': 'Space',
     'ralt': 'Alt', 'rctrl': 'Ctrl'
 }
@@ -65,10 +65,6 @@ def on_press(key):
             key_char = "ctrl"
         elif key == keyboard.Key.alt_l:
             key_char = "alt"
-        elif key == keyboard.Key.minus:
-            key_char = "-"
-        elif key == keyboard.Key.equal:
-            key_char = "="
 
         else:
             key_char = str(key).split('.')[1]
@@ -93,11 +89,11 @@ def update_heatmap():
         # ... (first row of keys)
         'tilde': (0, 5), '1': (1, 5), '2': (2, 5), '3': (3, 5), '4': (4, 5), '5': (5, 5), '6': (6, 5), '7': (7, 5), '8': (8, 5), '9': (9, 5), '0': (10, 5), '-': (11, 5), '=': (12, 5), 'backspace': (13, 5),
         # ... (second row of keys)
-        'tab': (0, 4), 'q': (1.5, 4), 'w': (2.5, 4), 'e': (3.5, 4), 'r': (4.5, 4), 't': (5.5, 4), 'y': (6.5, 4), 'u': (7.5, 4), 'i': (8.5, 4), 'o': (9.5, 4), 'p': (10.5, 4), 'lbracket': (11.5, 4), 'rbracket': (12.5, 4), 'backslash': (13.5, 4),
+        'tab': (0, 4), 'q': (1.5, 4), 'w': (2.5, 4), 'e': (3.5, 4), 'r': (4.5, 4), 't': (5.5, 4), 'y': (6.5, 4), 'u': (7.5, 4), 'i': (8.5, 4), 'o': (9.5, 4), 'p': (10.5, 4), '[': (11.5, 4), ']': (12.5, 4), '\\': (13.5, 4),
         # ... (third row of keys)
-        'capslock': (0.25, 3), 'a': (1.75, 3), 's': (2.75, 3), 'd': (3.75, 3), 'f': (4.75, 3), 'g': (5.75, 3), 'h': (6.75, 3), 'j': (7.75, 3), 'k': (8.75, 3), 'l': (9.75, 3), 'semicolon': (10.75, 3), 'quote': (11.75, 3), 'enter': (12.75, 3),
+        'capslock': (0.25, 3), 'a': (1.75, 3), 's': (2.75, 3), 'd': (3.75, 3), 'f': (4.75, 3), 'g': (5.75, 3), 'h': (6.75, 3), 'j': (7.75, 3), 'k': (8.75, 3), 'l': (9.75, 3), ';': (10.75, 3), '\'': (11.75, 3), 'enter': (12.75, 3),
         # ... (fourth row of keys)
-        'shift': (0, 2), 'z': (2, 2), 'x': (3, 2), 'c': (4, 2), 'v': (5, 2), 'b': (6, 2), 'n': (7, 2), 'm': (8, 2), 'comma': (9, 2), 'period': (10, 2), 'slash': (11, 2), 'rshift': (12, 2),
+        'shift': (0, 2), 'z': (2, 2), 'x': (3, 2), 'c': (4, 2), 'v': (5, 2), 'b': (6, 2), 'n': (7, 2), 'm': (8, 2), ',': (9, 2), '.': (10, 2), '/': (11, 2), 'rshift': (12, 2),
         # ... (fifth row of keys)
         'ctrl': (0, 1), 'fn': (1, 1), 'win': (2, 1), 'alt': (3, 1), 'space': (8, 1), 'ralt': (13, 1), 'rctrl': (14, 1),
         # ... (sixth row of keys)
